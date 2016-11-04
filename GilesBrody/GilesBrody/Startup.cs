e8b13f36df071ccd2fbaf72c902eee15fb3d878f
SShaw1985 <stephen.shaw85@gmail.com>
2016-11-04 08:30:23 +0000

    Initial commit
    MVC, Web api and Umbraco

---
+++ GilesBrody/GilesBrody/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GilesBrody.Startup))]
+namespace GilesBrody
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
